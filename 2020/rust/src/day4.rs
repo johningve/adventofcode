8e771ddcbad1c2e8317cd7a98dbbb231afee6743
John Ingve Olsen <johningveolsen@gmail.com>
2021-01-08 21:27:58 +0100

    rust day4

--- 2020/rust/src/day4.rs
+++ 2020/rust/src/day4.rs
@@ -43,6 +43,7 @@ impl Passport {
             || self.iyr == 0
             || self.eyr == 0
             || self.hgt.is_empty()
+            || self.hcl.is_empty()
             || self.ecl.is_empty()
             || self.pid.is_empty()
         {
@@ -171,9 +172,45 @@ mod tests {
         hcl:#cfa07d eyr:2025 pid:166559648
         iyr:2011 ecl:brn hgt:59in
     "};
+
     #[test]
     fn part1() {
         let day4 = Day4::new(INPUT_PART1.lines().map(|l| l.to_owned()));
         assert_eq!(day4.solve_part1(), 2);
+    }
+
+    const INPUT_PART2: &str = indoc! {"
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007
+
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    "};
+
+    #[test]
+    fn part2() {
+        let day4 = Day4::new(INPUT_PART2.lines().map(|l| l.to_owned()));
+        assert_eq!(day4.solve_part2(), 4);
     }
 }
