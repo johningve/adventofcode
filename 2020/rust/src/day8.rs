359e1096231eec37aa965fe531cb846675608861
John Ingve Olsen <johningveolsen@gmail.com>
2021-01-10 13:52:34 +0100

    rust day8

--- 2020/rust/src/day8.rs
+++ 2020/rust/src/day8.rs
@@ -1,15 +1,17 @@
 use crate::solver::Solver;
+use std::collections::HashSet;
 
+#[derive(Clone, Copy)]
 enum Instruction {
     Acc(i64),
     Jmp(i64),
-    Nop,
+    Nop(i64),
 }
 
 #[derive(Default)]
 struct Handheld {
     instructions: Vec<Instruction>,
-    ip: usize,
+    ip: i64,
     acc: i64,
 }
 
@@ -31,7 +33,7 @@ impl Handheld {
             hh.instructions.push(match op {
                 "acc" => Instruction::Acc(arg.unwrap()),
                 "jmp" => Instruction::Jmp(arg.unwrap()),
-                "nop" => Instruction::Nop,
+                "nop" => Instruction::Nop(arg.unwrap()),
                 _ => continue,
             });
         }
@@ -39,27 +41,55 @@ impl Handheld {
     }
 
     fn execute_instruction(&mut self) {
-        let instruction = &self.instructions[self.ip];
+        let instruction = &self.instructions[self.ip as usize];
         match instruction {
             Instruction::Acc(arg) => self.acc += *arg,
-            Instruction::Jmp(arg) => self.ip += *arg as usize,
-            Instruction::Nop => (),
+            Instruction::Jmp(arg) => self.ip += *arg - 1,
+            Instruction::Nop(_) => (),
         }
+        self.ip += 1;
+    }
+
+    fn detect_infinite_loop(&mut self) -> bool {
+        self.acc = 0;
+        self.ip = 0;
+        let mut executed_set = HashSet::new();
+        while self.ip >= 0 && self.ip < self.instructions.len() as i64 {
+            if executed_set.contains(&self.ip) {
+                return true;
+            }
+            executed_set.insert(self.ip);
+            self.execute_instruction();
+        }
+        return false;
+    }
+
+    fn solve_part1(&mut self) -> i64 {
+        self.detect_infinite_loop();
+        self.acc
+    }
+
+    fn solve_part2(&mut self) -> i64 {
+        for i in 0..self.instructions.len() {
+            let instruction = &mut self.instructions[i];
+            let tmp = *instruction;
+            *instruction = match instruction {
+                Instruction::Nop(arg) => Instruction::Jmp(*arg),
+                Instruction::Jmp(arg) => Instruction::Nop(*arg),
+                _ => continue,
+            };
+            drop(instruction);
+            if !self.detect_infinite_loop() {
+                break;
+            }
+            self.instructions[i] = tmp;
+        }
+        self.acc
     }
 }
 
 pub struct Day8 {
     hh: Handheld,
-}
-
-impl Day8 {
-    fn solve_part1(&self) -> i64 {
-        -1
-    }
-
-    fn solve_part2(&self) -> i64 {
-        -1
-    }
 }
 
 impl Solver for Day8 {
@@ -70,6 +100,36 @@ impl Solver for Day8 {
     }
 
     fn solve(&mut self) -> (i64, i64) {
-        (self.solve_part1(), self.solve_part2())
+        (self.hh.solve_part1(), self.hh.solve_part2())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    const INPUT: &str = &indoc! {"
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+    "};
+
+    #[test]
+    fn part1() {
+        let mut solution = Day8::new(INPUT.lines().map(|l| l.to_owned()));
+        assert_eq!(solution.solve().0, 5);
+    }
+
+    #[test]
+    fn part2() {
+        let mut solution = Day8::new(INPUT.lines().map(|l| l.to_owned()));
+        assert_eq!(solution.solve().1, 8);
     }
 }
