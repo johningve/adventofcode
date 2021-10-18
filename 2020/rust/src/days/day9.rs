30ae0813b5517ca00db6579a3758b88e9520881f
John Ingve Olsen <johningveolsen@gmail.com>
2021-10-18 14:27:33 +0200

    2020 day 9 (rust) - added test for part 2

--- 2020/rust/src/days/day9.rs
+++ 2020/rust/src/days/day9.rs
@@ -79,20 +79,31 @@ impl Solver for Day9 {
 mod tests {
     use super::Day9;
 
+    const INPUT: &[i64] = &[
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,
+    ];
+
     #[test]
     fn part1() {
-        let numbers = vec![
-            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
-            576,
-        ];
-
         assert_eq!(
             Day9 {
-                numbers,
+                numbers: INPUT.to_vec(),
                 preamble_len: 5
             }
             .solve_part1(),
             127
         );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            Day9 {
+                numbers: INPUT.to_vec(),
+                preamble_len: 5
+            }
+            .solve_part2(),
+            62
+        )
     }
 }
